10a5bd37c7342b92152044ffbc728a4c5d200a5e
Boris Nagaev <bnagaev@gmail.com>
2014-12-21 16:25:22 +0300

    Sequence: divide metatables of class and instance

--- npge/model/Sequence.lua
+++ npge/model/Sequence.lua
@@ -1,8 +1,10 @@
 
 local Sequence = {}
-
 local Sequence_mt = {}
+local seq_mt = {}
+
 Sequence_mt.__index = Sequence_mt
+seq_mt.__index = seq_mt
 
 Sequence_mt.__call = function(self, name, text, description)
     assert(type(name) == 'string')
@@ -13,7 +15,7 @@ Sequence_mt.__call = function(self, name, text, description)
     seq._name = name
     seq._text = Sequence.to_atgcn(text)
     seq._description = description
-    return setmetatable(seq, Sequence_mt)
+    return setmetatable(seq, seq_mt)
 end
 
 Sequence_mt.to_atgcn = function(text)
@@ -23,11 +25,11 @@ Sequence_mt.to_atgcn = function(text)
         :gsub('[^ATGCN]', '')
 end
 
-Sequence_mt.type = function()
+seq_mt.type = function()
     return 'Sequence'
 end
 
-Sequence_mt.name = function(self)
+seq_mt.name = function(self)
     return self._name
 end
 
@@ -39,34 +41,34 @@ local function get_name_part(name, number)
     end
 end
 
-Sequence_mt.genome = function(self)
+seq_mt.genome = function(self)
     return get_name_part(self._name, 1)
 end
 
-Sequence_mt.chromosome = function(self)
+seq_mt.chromosome = function(self)
     return get_name_part(self._name, 2)
 end
 
-Sequence_mt.circularity = function(self)
+seq_mt.circularity = function(self)
     local circularity = get_name_part(self._name, 3)
     if circularity == 'c' or circularity == 'l' then
         return circularity
     end
 end
 
-Sequence_mt.text = function(self)
+seq_mt.text = function(self)
     return self._text
 end
 
-Sequence_mt.description = function(self)
+seq_mt.description = function(self)
     return self._description
 end
 
-Sequence_mt.size = function(self)
+seq_mt.size = function(self)
     return #self._text
 end
 
-Sequence_mt.at = function(self, index)
+seq_mt.at = function(self, index)
     return self._text:sub(index + 1, index + 1)
 end
 
