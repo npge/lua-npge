fc7f19a624ba9243ef5428c243e6bd09ddf69eb8
Boris Nagaev <bnagaev@gmail.com>
2014-12-21 16:33:08 +0300

    use require 'npge.model' in fragment_spec

--- spec/model/fragment_spec.lua
+++ spec/model/fragment_spec.lua
@@ -1,10 +1,9 @@
-local Fragment = require 'npge.model.Fragment'
-local Sequence = require 'npge.model.Sequence'
+local model = require 'npge.model'
 
 describe("model.fragment", function()
     it("creates fragment", function()
-        local s = Sequence("test_name", "ATGC")
-        local f = Fragment(s, 0, 3, 1)
+        local s = model.Sequence("test_name", "ATGC")
+        local f = model.Fragment(s, 0, 3, 1)
         assert.are.equal(f:seq(), s)
         assert.are.equal(f:start(), 0)
         assert.are.equal(f:stop(), 3)
