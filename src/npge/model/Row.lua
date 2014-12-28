6f2dbedfec75329e78b2dbe1d8cf36b8d077efe5
Boris Nagaev <bnagaev@gmail.com>
2014-12-28 15:47:23 +0300

    implement Row
    the first version

---
+++ src/npge/model/Row.lua
@@ -0,0 +1,58 @@
+
+local Row = {}
+local Row_mt = {}
+local row_mt = {}
+
+Row_mt.__index = Row_mt
+row_mt.__index = row_mt
+
+Row_mt.__call = function(self, text)
+    local row = {}
+    row._text = text
+    return setmetatable(row, row_mt)
+end
+
+row_mt.length = function(self)
+    return #self._text
+end
+
+row_mt.block2fragment = function(self, blockpos)
+    local text = self._text
+    local size = #text
+    assert(blockpos >= 0)
+    assert(blockpos < size)
+    local char = text:sub(blockpos + 1, blockpos + 1)
+    if char == '-' then
+        return -1
+    end
+    local fragmentpos1 = 0
+    for blockpos1 = 0, size - 1 do
+        local char = text:sub(blockpos1 + 1, blockpos1 + 1)
+        if char ~= '-' then
+            if blockpos1 == blockpos then
+                return fragmentpos1
+            end
+            fragmentpos1 = fragmentpos1 + 1
+        end
+    end
+end
+
+row_mt.fragment2block = function(self, fragmentpos)
+    local text = self._text
+    local size = #text
+    assert(fragmentpos >= 0)
+    local fragmentpos1 = 0
+    for blockpos1 = 0, size - 1 do
+        local char = text:sub(blockpos1 + 1, blockpos1 + 1)
+        if char ~= '-' then
+            if fragmentpos1 == fragmentpos then
+                return blockpos1
+            end
+            fragmentpos1 = fragmentpos1 + 1
+        end
+    end
+    error("No such fragment pos in the row: " .. fragmentpos)
+end
+
+return setmetatable(Row, Row_mt)
+
--- src/npge/model/init.lua
+++ src/npge/model/init.lua
@@ -2,5 +2,6 @@ return {
     Sequence = require 'npge.model.Sequence',
     Fragment = require 'npge.model.Fragment',
     Block = require 'npge.model.Block',
+    Row = require 'npge.model.Row',
 }
 
