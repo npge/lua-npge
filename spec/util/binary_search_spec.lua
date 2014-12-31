997e27a82c3b35e1e021944025f54f6f8214d11d
Boris Nagaev <bnagaev@gmail.com>
2014-12-31 14:37:01 +0300

    few more tests for binary_search.lower

--- spec/util/binary_search_spec.lua
+++ spec/util/binary_search_spec.lua
@@ -3,9 +3,12 @@ local binary_search = require 'npge.util.binary_search'
 describe("util.binary_search", function()
     it("checks that binary search works (lower)", function()
         local lower = binary_search.lower
+        assert.equal(lower({1, 2, 3}, 0), 1)
         assert.equal(lower({1, 2, 3}, 1), 1)
         assert.equal(lower({1, 2, 3}, 2), 2)
         assert.equal(lower({1, 2, 3}, 3), 3)
+        assert.equal(lower({1, 2, 3}, 4), 4)
+        assert.equal(lower({1, 2, 3}, 5), 4)
         --
         assert.equal(lower({1, 2, 2}, 2), 2)
         assert.equal(lower({1, 2, 2}, 1.5), 2)
@@ -14,9 +17,12 @@ describe("util.binary_search", function()
 
     it("checks that binary search works (upper)", function()
         local upper = binary_search.upper
+        assert.equal(upper({1, 2, 3}, 0), 1)
         assert.equal(upper({1, 2, 3}, 1), 2)
         assert.equal(upper({1, 2, 3}, 2), 3)
         assert.equal(upper({1, 2, 3}, 3), 4)
+        assert.equal(upper({1, 2, 3}, 4), 4)
+        assert.equal(upper({1, 2, 3}, 5), 4)
         --
         assert.equal(upper({1, 2, 2}, 2), 4)
         assert.equal(upper({1, 2, 2}, 1.5), 2)
