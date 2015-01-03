6b6c2dcc17437d7d99a014720a39d9837007c149
Boris Nagaev <bnagaev@gmail.com>
2015-01-03 17:00:44 +0300

    functions to make array/dict from iterator

--- spec/util/clone_spec.lua
+++ spec/util/clone_spec.lua
@@ -7,11 +7,32 @@ describe("util.clone", function()
         assert.not_equal(clone.array(x), x)
     end)
 
+    it("makes an array from an iterator", function()
+        local i = 0
+        local it = function()
+            i = i + 1
+            if i < 5 then
+                return i
+            end
+        end
+        assert.same(clone.array_from_it(it), {1, 2, 3, 4})
+    end)
+
     it("makes a copy of a dict", function()
         local x = {a = 1, b = 'c', [1] = 0}
         assert.same(clone.dict(x), x)
         assert.not_equal(clone.dict(x), x)
     end)
-end)
 
+    it("makes a dict from an iterator", function()
+        local i = 0
+        local it = function()
+            i = i + 1
+            if i < 3 then
+                return 'k' .. i, i
+            end
+        end
+        assert.same(clone.dict_from_it(it), {k1 = 1, k2 = 2})
+    end)
+end)
 
--- src/npge/util/clone.lua
+++ src/npge/util/clone.lua
@@ -7,9 +7,25 @@ return {
         return clone
     end,
 
+    array_from_it = function(it)
+        local clone = {}
+        for item in it do
+            table.insert(clone, item)
+        end
+        return clone
+    end,
+
     dict = function(x)
         local clone = {}
         for key, value in pairs(x) do
+            clone[key] = value
+        end
+        return clone
+    end,
+
+    dict_from_it = function(it)
+        local clone = {}
+        for key, value in it do
             clone[key] = value
         end
         return clone
