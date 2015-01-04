a599eed905eee58d0e8644b608ffc95c4744dd0b
Boris Nagaev <bnagaev@gmail.com>
2015-01-04 19:03:37 +0300

    add method BlockSet:tolua

--- spec/model/blockset_spec.lua
+++ spec/model/blockset_spec.lua
@@ -289,5 +289,21 @@ describe("model.blockset", function()
         end
         assert.equal(make_bs(), make_bs())
     end)
+
+    it("serializes blocksets", function()
+        local B = function(...)
+            return model.Block({...})
+        end
+        local BS = model.BlockSet
+        local s1 = model.Sequence("g1&c&c", "ATAT")
+        local s2 = model.Sequence("g2&c&c", "ATAT")
+        local f1 = model.Fragment(s1, 0, 0, 1)
+        local f1a = model.Fragment(s1, 1, 1, 1)
+        local f2 = model.Fragment(s2, 0, 0, 1)
+        local bs1 = BS({s1, s2}, {B(f1, f2), B(f1a)})
+        local lua = bs1:tolua()
+        local bs2 = loadstring(lua)()
+        assert.equal(bs1, bs2)
+    end)
 end)
 
--- src/npge/model/BlockSet.lua
+++ src/npge/model/BlockSet.lua
@@ -274,5 +274,73 @@ bs_mt.prev = function(self, fragment)
     return parent_or_fragment(self, f)
 end
 
+bs_mt.tolua = function(self)
+    local as_lines = function(text)
+        local result = {}
+        local step = 60
+        for i = 0, #text - 1, step do
+            table.insert(result, text:sub(i, i + step - 1))
+        end
+        return table.concat(result, "\n")
+    end
+    local seq_to_lua = function(seq)
+        local text = as_lines(seq:text())
+        local lua = "Sequence(%q, %q, %q)"
+        return lua:format(seq:name(), text, seq:description())
+    end
+    local seqs_to_lua = function(seqs)
+        local seqs_list = {}
+        for _, seq in ipairs(seqs) do
+            local lua = "[%q] = %s"
+            lua = lua:format(seq:name(), seq_to_lua(seq))
+            table.insert(seqs_list, lua)
+        end
+        local seqs_str = table.concat(seqs_list, ',')
+        return ("{%s}"):format(seqs_str)
+    end
+    local fragment_to_lua = function(fragment)
+        local lua = "Fragment(name2seq[%q], %i, %i, %i)"
+        return lua:format(fragment:sequence():name(),
+            fragment:start(), fragment:stop(), fragment:ori())
+    end
+    local block_to_lua = function(block)
+        local ff = {}
+        for fragment in block:iter_fragments() do
+            local text = block:text(fragment)
+            text = as_lines(text)
+            local fragment_str = fragment_to_lua(fragment)
+            local lua = "{%s, %q}"
+            table.insert(ff, lua:format(fragment_str, text))
+        end
+        ff = table.concat(ff, ', ')
+        local lua = "Block({%s})"
+        return lua:format(ff)
+    end
+    local blocks_to_lua = function(blocks)
+        local blocks_list = {}
+        for _, block in ipairs(blocks) do
+            table.insert(blocks_list, block_to_lua(block))
+        end
+        local blocks_str = table.concat(blocks_list, ',')
+        return ("{%s}"):format(blocks_str)
+    end
+    local lua = [[do
+    local Sequence = require 'npge.model.Sequence'
+    local Fragment = require 'npge.model.Fragment'
+    local Block = require 'npge.model.Block'
+    local BlockSet = require 'npge.model.BlockSet'
+    local name2seq = %s
+    local blocks = %s
+    local seqs = {}
+    for name, seq in pairs(name2seq) do
+        table.insert(seqs, seq)
+    end
+    return BlockSet(seqs, blocks)
+end]]
+    return lua:format(
+        seqs_to_lua(self:sequences()),
+        blocks_to_lua(self:blocks()))
+end
+
 return setmetatable(BlockSet, BlockSet_mt)
 
