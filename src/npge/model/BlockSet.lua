eca7513b088c618b11e2592fc9fc35b8d7757e56
Boris Nagaev <bnagaev@gmail.com>
2015-01-05 17:52:55 +0300

    BlockSet:tolua: first line is not cutted
    Was:
    123
    4567
    890a
    
    Become:
    1234
    5678
    90a

--- src/npge/model/BlockSet.lua
+++ src/npge/model/BlockSet.lua
@@ -278,7 +278,7 @@ bs_mt.tolua = function(self)
     local as_lines = function(text)
         local result = {}
         local step = 50
-        for i = 0, #text, step do
+        for i = 1, #text, step do
             table.insert(result, text:sub(i, i + step - 1))
         end
         return table.concat(result, "\n")
