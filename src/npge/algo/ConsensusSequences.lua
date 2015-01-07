1976769213ae327d9b7ad29fac01fb74f4bdf585
Boris Nagaev <bnagaev@gmail.com>
2015-01-08 00:00:12 +0300

    ConsensusSequences: optimization if block=sequence
    If the only fragment of this block covers whole sequence,
    then consensus sequence = original sequence.

--- src/npge/algo/ConsensusSequences.lua
+++ src/npge/algo/ConsensusSequences.lua
@@ -2,11 +2,25 @@ return function(blockset)
     local seq2block = {}
     local sequences = {}
     for i, block in ipairs(blockset:blocks()) do
-        local consensus = require 'npge.block.consensus'
-        local text = consensus(block)
-        local name = ('consensus%06d'):format(i)
-        local Sequence = require 'npge.model.Sequence'
-        local seq = Sequence(name, text)
+        local seq
+        if block:size() == 1 then
+            local Fragment = require 'npge.model.Fragment'
+            local fragment = block:fragments()[1]
+            local seq0 = fragment:sequence()
+            local f =  Fragment(seq0, 0, seq0:length() - 1, 1)
+            if f == fragment then
+                -- the only fragment of this block
+                -- covers whole sequence
+                seq = seq0
+            end
+        end
+        if not seq then
+            local consensus = require 'npge.block.consensus'
+            local text = consensus(block)
+            local name = ('consensus%06d'):format(i)
+            local Sequence = require 'npge.model.Sequence'
+            seq = Sequence(name, text)
+        end
         seq2block[seq] = block
         table.insert(sequences, seq)
     end
