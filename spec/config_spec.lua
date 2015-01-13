7f7c8e0f6b067a9e1a3772d4006be6b8bba2d39e
Boris Nagaev <bnagaev@gmail.com>
2015-01-14 00:07:10 +0300

    config: require >= 1 identical end positions

--- spec/config_spec.lua
+++ spec/config_spec.lua
@@ -5,7 +5,7 @@ describe("config", function()
         assert.truthy(config.general.MIN_IDENTITY <= 1)
         assert.truthy(config.general.MIN_LENGTH >= 1)
         assert.truthy(
-            config.general.MIN_END_IDENTICAL_COLUMNS >= 0)
+            config.general.MIN_END_IDENTICAL_COLUMNS >= 1)
         assert.truthy(type(config.blast.DUST), 'boolean')
         assert.truthy(config.blast.EVALUE >= 0)
         assert.truthy(config.blast.MAX_NS >= 0)
