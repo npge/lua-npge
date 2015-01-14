5e43bf425cd74af1b29d870833a2bad2cd1c850a
Boris Nagaev <bnagaev@gmail.com>
2015-01-15 02:03:24 +0300

    config: load values from local file npge.conf
    npge.conf is executed in a sandbox

--- spec/config_spec.lua
+++ spec/config_spec.lua
@@ -10,4 +10,60 @@ describe("config", function()
         assert.truthy(config.blast.EVALUE >= 0)
         assert.truthy(config.blast.MAX_NS >= 0)
     end)
+
+    local prepare_config = function(conf)
+        -- unload 'npge.config'
+        package.loaded['npge.config'] = nil
+        -- rename npge.conf, if exists
+        local file_exists = require 'npge.util.file_exists'
+        if file_exists('npge.conf') then
+            os.rename('npge.conf', 'npge.conf-bak-by-busted')
+        end
+        local f = io.open('npge.conf', 'w')
+        f:write(conf)
+        f:close()
+    end
+
+    local restore_config = function()
+        -- unload config
+        package.loaded['npge.config'] = nil
+        -- remove testing npge.conf
+        os.remove('npge.conf')
+        -- recover previous npge.conf if it exsted
+        local file_exists = require 'npge.util.file_exists'
+        if file_exists('npge.conf-bak-by-busted') then
+            os.rename('npge.conf-bak-by-busted', 'npge.conf')
+        end
+    end
+
+    it("reads npge.conf", function()
+        prepare_config 'general.MIN_LENGTH = 250\n'
+        -- load config
+        local config = require 'npge.config'
+        assert.equal(config.general.MIN_LENGTH, 250)
+        -- unload config
+        restore_config()
+    end)
+
+    local check_error = function(conf)
+        return function()
+            prepare_config(conf)
+            -- load config
+            assert.has_error(function()
+                local config = require 'npge.config'
+            end)
+            -- unload config
+            restore_config()
+            print = orig_print
+        end
+    end
+
+    it("prints error messages if errors in config (syntax)",
+        check_error('a = 1; a++\n'))
+
+    it("prints error messages if errors in config (runtime)",
+        check_error('a = 1; a()\n'))
+
+    it("prints error messages if errors in config (type)",
+        check_error('general.MIN_LENGTH = true\n'))
 end)
--- src/npge/config.lua
+++ src/npge/config.lua
@@ -1,4 +1,4 @@
-return {
+local config = {
     general = {
         -- Minimum acceptable identity of block (0.9 is 90%)
         MIN_IDENTITY = 0.9,
@@ -20,3 +20,41 @@ return {
         MAX_NS = 3,
     }
 }
+
+local update_keys = function(env)
+    for section_name, section in pairs(config) do
+        if env[section_name] then
+            local env_section = env[section_name]
+            for name, value in pairs(env_section) do
+                if type(value) == type(section[name]) then
+                    section[name] = value
+                else
+                    local msg = 'Ignore %s.%s from npge.conf'
+                    error(msg:format(section_name, name))
+                end
+            end
+        end
+    end
+end
+
+local file_exists = require 'npge.util.file_exists'
+if file_exists('npge.conf') then
+    local conf_file = io.open('npge.conf')
+    local conf = conf_file:read('*a')
+    conf_file:close()
+    local sandbox = require 'npge.util.sandbox'
+    local env = {general={}, blast={}}
+    local conf_sandboxed, message = sandbox(env, conf)
+    if conf_sandboxed then
+        local status = pcall(conf_sandboxed)
+        if status then
+            update_keys(env)
+        else
+            error('Failed to run commands found in config')
+        end
+    else
+        error('Failed to read config: ' .. message)
+    end
+end
+
+return config
