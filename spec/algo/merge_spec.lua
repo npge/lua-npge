3950565ba4a490433d18809408c2ddca3f21d7b9
Boris Nagaev <bnagaev@gmail.com>
2015-01-16 13:56:09 +0300

    Merge accepts any number of blocksets

--- spec/algo/merge_spec.lua
+++ spec/algo/merge_spec.lua
@@ -14,4 +14,32 @@ describe("algo.Merge", function()
         assert.same(sum:fragments(s), {f1, f2})
         assert.equal(sum:size(), 2)
     end)
+
+    it("merges 3 blocksets", function()
+        local model = require 'npge.model'
+        local s = model.Sequence("s", "ATAT")
+        local f1 = model.Fragment(s, 0, 0, 1)
+        local b1 = model.Block({f1})
+        local bs1 = model.BlockSet({s}, {b1})
+        local f2 = model.Fragment(s, 1, 1, 1)
+        local b2 = model.Block({f2})
+        local bs2 = model.BlockSet({s}, {b2})
+        local f3 = model.Fragment(s, 2, 2, 1)
+        local b3 = model.Block({f3})
+        local bs3 = model.BlockSet({s}, {b3})
+        local Merge = require 'npge.algo.Merge'
+        local sum = Merge(bs1, bs2, bs3)
+        assert.equal(sum, model.BlockSet({s}, {b1, b2, b3}))
+    end)
+
+    it("merges 1 blocksets (return the argument)", function()
+        local model = require 'npge.model'
+        local s = model.Sequence("s", "ATAT")
+        local f1 = model.Fragment(s, 0, 0, 1)
+        local b1 = model.Block({f1})
+        local bs1 = model.BlockSet({s}, {b1})
+        local Merge = require 'npge.algo.Merge'
+        local sum = Merge(bs1)
+        assert.equal(sum, model.BlockSet({s}, {b1}))
+    end)
 end)
--- src/npge/algo/Merge.lua
+++ src/npge/algo/Merge.lua
@@ -1,9 +1,17 @@
-return function(bs1, bs2)
-    assert(bs1:type() == 'BlockSet')
-    assert(bs2:type() == 'BlockSet')
-    assert(bs1:same_sequences(bs2))
+return function(...)
+    local blocksets = {...}
+    local bs1 = blocksets[1]
+    for _, bs in ipairs(blocksets) do
+        assert(bs:type() == 'BlockSet')
+        assert(bs:same_sequences(bs1))
+    end
+    local lists_of_blocks = {}
+    for _, bs in ipairs(blocksets) do
+        table.insert(lists_of_blocks, bs:blocks())
+    end
     local concat_arrays = require 'npge.util.concat_arrays'
-    local blocks = concat_arrays(bs1:blocks(), bs2:blocks())
+    local unpack = require 'npge.util.unpack'
+    local blocks = concat_arrays(unpack(lists_of_blocks))
     local BlockSet = require 'npge.model.BlockSet'
     return BlockSet(bs1:sequences(), blocks)
 end
