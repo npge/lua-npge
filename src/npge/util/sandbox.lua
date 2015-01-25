6cf49310be2bfb700e1eb12510236501cb79de32
Boris Nagaev <bnagaev@gmail.com>
2015-01-25 23:58:42 +0300

    comment out non-tested code (sandbox for Lua 5.2)

--- src/npge/util/sandbox.lua
+++ src/npge/util/sandbox.lua
@@ -15,7 +15,7 @@ return function(env, code)
         'Implemented in Lua 5.1 and 5.2 only')
     if _VERSION == 'Lua 5.2' then
         -- not tested in Lua 5.1 and LuaJIT
-        return load(code, 'sandbox', 't', env)
+        -- return load(code, 'sandbox', 't', env)
     elseif _VERSION == 'Lua 5.1' then
         local f, message = loadstring(code, 'sandbox')
         if not f then
