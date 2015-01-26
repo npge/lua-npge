2d9cfc5417b8e654479c27f1889fab635fffcd22
Boris Nagaev <bnagaev@gmail.com>
2015-01-26 13:07:05 +0300

    PangenomeMaker: use BlastHitsUnwound

--- src/npge/algo/PangenomeMaker.lua
+++ src/npge/algo/PangenomeMaker.lua
@@ -12,13 +12,7 @@ return function(bs)
         end
         local bs_covered = algo.Cover(bs)
         -- blast
-        local consensuses, seq2block =
-            algo.ConsensusSequences(bs_covered)
-        local hits_cons = algo.BlastHits(consensuses)
-        hits_cons = algo.Cover(hits_cons)
-        local hits = algo.UnwindBlocks(hits_cons,
-            bs_covered, seq2block)
-        hits = algo.GoodSubblocks(hits)
+        local hits = algo.BlastHitsUnwound(bs_covered)
         bs1 = algo.BlocksWithoutOverlaps(bs, hits)
         -- join
         local joined = algo.Join(bs1)
