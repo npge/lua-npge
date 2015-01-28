46723deb3ca08fa828bcc60f9b30ef76c9ca7e30
Boris Nagaev <bnagaev@gmail.com>
2015-01-29 00:53:47 +0300

    optimise block.is_good
    Do not create temp blocks for beginning and ending
    using slice, but tell start and stop column numbers to
    function alignment.identity().

--- src/npge/block/is_good.lua
+++ src/npge/block/is_good.lua
@@ -1,8 +1,7 @@
-local find_gap = function(block, min_length)
-    for fragment in block:iter_fragments() do
-        local row = block:text(fragment)
+local find_gap = function(rows, length, min_length)
+    for _, row in ipairs(rows) do
         local gap_length = 0
-        for i = 1, block:length() do
+        for i = 1, length do
             if row:sub(i, i) == '-' then
                 gap_length = gap_length + 1
                 if gap_length >= min_length then
@@ -35,35 +34,33 @@ return function(block)
     if block:length() < min_length then
         return false, 'length', block:length()
     end
+    -- make rows
+    local rows = {}
+    for fragment in block:iter_fragments() do
+        table.insert(rows, block:text(fragment))
+    end
     -- check identity
     local min_ident = config.general.MIN_IDENTITY
-    local identity = require 'npge.block.identity'
-    local ident = identity(block)
-    if identity.less(ident, min_ident) then
+    local identity_less = (require 'npge.block.identity').less
+    local identity = require 'npge.alignment.identity'
+    local ident = identity(rows)
+    if identity_less(ident, min_ident) then
         return false, 'identity', ident
     end
     -- check identity of end subblocks
-    local slice = require 'npge.block.slice'
     local min_cols = config.general.MIN_END_IDENTICAL_COLUMNS
-    local beginning = slice(block, 0, min_cols - 1)
-    if not beginning then
-        return false, 'beginning identity', 0
-    end
-    local ident = identity(beginning)
-    if identity.less(ident, 1.0) then
+    local ident = identity(rows, 0, min_cols - 1)
+    if identity_less(ident, 1.0) then
         return false, 'beginning identity', ident
     end
-    local ending = slice(block, block:length() - min_cols,
+    local ident = identity(rows, block:length() - min_cols,
         block:length() - 1)
-    if not ending then
-        return false, 'ending identity', 0
-    end
-    local ident = identity(ending)
-    if identity.less(ident, 1.0) then
+    if identity_less(ident, 1.0) then
         return false, 'ending identity', ident
     end
     -- check gap length
-    local has_long_gap, long_gap = find_gap(block, min_length)
+    local has_long_gap, long_gap = find_gap(rows,
+        block:length(), min_length)
     if has_long_gap then
         return false, 'long gaps', long_gap
     end
