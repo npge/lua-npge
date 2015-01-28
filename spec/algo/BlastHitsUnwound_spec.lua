a4b32770cc97969e55c8e0b3a1188a11b034f126
Boris Nagaev <bnagaev@gmail.com>
2015-01-28 04:32:28 +0300

    BlastHitsUnwound has options, notably bank

--- spec/algo/BlastHitsUnwound_spec.lua
+++ spec/algo/BlastHitsUnwound_spec.lua
@@ -13,6 +13,21 @@ describe("algo.BlastHitsUnwound", function()
         assert.truthy(#hits:blocks() > 0)
     end)
 
+    it("finds hits using blast+ on blocks (different bank)",
+    function()
+        local Sequence = require 'npge.model.Sequence'
+        local s1 = Sequence('s1', string.rep('ATGC', 100))
+        local s2 = Sequence('s2', string.rep('ATGC', 100))
+        local BlockSet = require 'npge.model.BlockSet'
+        local Cover = require 'npge.algo.Cover'
+        local query = Cover(BlockSet({s1}, {}))
+        local bank = Cover(BlockSet({s2}, {}))
+        local BlastHitsUnwound =
+            require 'npge.algo.BlastHitsUnwound'
+        local hits = BlastHitsUnwound(query, {bank=bank})
+        assert.truthy(#hits:blocks() > 0)
+    end)
+
     it("finds nothing if no blocks", function()
         local Sequence = require 'npge.model.Sequence'
         local s1 = Sequence('s1', string.rep('ATGC', 100))
--- src/npge/algo/BlastHitsUnwound.lua
+++ src/npge/algo/BlastHitsUnwound.lua
@@ -1,8 +1,17 @@
-return function(blockset)
+return function(blockset, options)
     local algo = require 'npge.algo'
     local consensuses, seq2block =
         algo.ConsensusSequences(blockset)
-    local hits_cons = algo.BlastHits(consensuses)
+    local orig_bank = options and options.bank
+    if orig_bank then
+        local s2b
+        options.bank, s2b = algo.ConsensusSequences(orig_bank)
+        for seq, block in pairs(s2b) do
+            seq2block[seq] = block
+        end
+        blockset = algo.Merge(blockset, orig_bank)
+    end
+    local hits_cons = algo.BlastHits(consensuses, options)
     hits_cons = algo.Cover(hits_cons)
     local hits = algo.UnwindBlocks(hits_cons,
         blockset, seq2block)
