3682ba97364eff39b36e86406388739f4bb96dcf
Boris Nagaev <bnagaev@gmail.com>
2015-02-01 16:21:48 +0300

    example of loading sequences, then blocks

--- spec/algo/LoadFromLua_spec.lua
+++ spec/algo/LoadFromLua_spec.lua
@@ -14,4 +14,25 @@ describe("algo.LoadFromLua", function()
         local blockset1 = LoadFromLua(lua)()
         assert.equal(blockset1, blockset)
     end)
+
+    it("loads sequences + blockset", function()
+        local model = require 'npge.model'
+        local s = model.Sequence("test_name", "ATAT")
+        local f1 = model.Fragment(s, 0, 1, 1)
+        local f2 = model.Fragment(s, 3, 2, -1)
+        local block1 = model.Block({f2})
+        local block2 = model.Block({f1})
+        local blockset = model.BlockSet({s}, {block1, block2})
+        local readIt = require 'npge.util.readIt'
+        local SequencesToLua =
+            require 'npge.algo.SequencesToLua'
+        local lua1 = readIt(SequencesToLua(blockset))
+        local BlockSetToLua = require 'npge.algo.BlockSetToLua'
+        local lua2 = readIt(BlockSetToLua(blockset))
+        local LoadFromLua = require 'npge.algo.LoadFromLua'
+        local enable_fromRef = true
+        local name2seq = LoadFromLua(lua1, enable_fromRef)()
+        local blockset1 = LoadFromLua(lua2)(name2seq)
+        assert.equal(blockset1, blockset)
+    end)
 end)
