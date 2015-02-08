ac178c39d4837cf72d8ae14ca9f619d8ad1a8182
Boris Nagaev <bnagaev@gmail.com>
2015-02-08 22:22:55 +0300

    to_atgcn_[and_gap], complement: test empty string

--- spec/alignment/complement_spec.lua
+++ spec/alignment/complement_spec.lua
@@ -2,5 +2,6 @@ describe("alignment.complement", function()
     it("calculates complement sequence", function()
         local complement = require 'npge.alignment.complement'
         assert.are.equal(complement("ATGC"), "GCAT")
+        assert.are.equal(complement(""), "")
     end)
 end)
