d18d5e389bbfe9cba3cf1a22ff30d1f48ed3373e
Boris Nagaev <bnagaev@gmail.com>
2015-02-13 16:12:38 +0300

    AddGoodBlast: add option "subset"

--- spec/algo/AddGoodBlast_spec.lua
+++ spec/algo/AddGoodBlast_spec.lua
@@ -52,4 +52,30 @@ describe("algo.AddGoodBlast", function()
         local hits = AddGoodBlast(bs_with_seqs, bs_with_seqs)
         assert.equal(#hits:blocks(), 0)
     end)
+
+    it("finds if query is a subset of bank", function()
+        local m = require 'npge.model'
+        local s1 = m.Sequence('s1', string.rep('ATGC', 100))
+        local b1 = m.Block({m.Fragment(s1, 0, 100, 1)})
+        local b2 = m.Block({m.Fragment(s1, 200, 101, -1)})
+        local query = m.BlockSet({s1}, {b1})
+        local bank = m.BlockSet({s1}, {b1, b2})
+        local AddGoodBlast = require 'npge.algo.AddGoodBlast'
+        local hits = AddGoodBlast(query, bank, {subset=1})
+        assert.truthy(#hits:blocks() >= 1)
+    end)
+
+    it("throws if query isn't bank subset, but declared to",
+    function()
+        local m = require 'npge.model'
+        local s1 = m.Sequence('s1', string.rep('ATGC', 100))
+        local b1 = m.Block({m.Fragment(s1, 0, 100, 1)})
+        local b2 = m.Block({m.Fragment(s1, 200, 101, -1)})
+        local query = m.BlockSet({s1}, {b1})
+        local bank = m.BlockSet({s1}, {b2})
+        local AddGoodBlast = require 'npge.algo.AddGoodBlast'
+        assert.has_error(function()
+            local hits = AddGoodBlast(query, bank, {subset=1})
+        end)
+    end)
 end)
--- src/npge/algo/AddGoodBlast.lua
+++ src/npge/algo/AddGoodBlast.lua
@@ -1,17 +1,34 @@
+-- Options are same as options of BlastHits
+-- Additional options:
+--  - subset (query is a subset of subject)
 return function(query, bank, options)
+    options = options or {}
     local algo = require 'npge.algo'
-    local query_cons, seq2block =
-        algo.ConsensusSequences(query, 'query')
+    local bank_cons, seq2block =
+        algo.ConsensusSequences(bank, 'bank')
     local s2b
-    local bank_cons, blockset
+    local query_cons, blockset
     if query == bank then
-        bank_cons = query_cons
-        blockset = query
+        query_cons = bank_cons
+        blockset = bank
+    elseif options.subset then
+        blockset = bank
+        local b2s = {}
+        for seq, block in pairs(seq2block) do
+            b2s[block] = seq
+        end
+        local query_seqs = {}
+        for block in query:iter_blocks() do
+            local seq = assert(b2s[block])
+            table.insert(query_seqs, seq)
+        end
+        local BlockSet = require 'npge.model.BlockSet'
+        query_cons = BlockSet(query_seqs, {})
     else
-        local bank_s2b
-        bank_cons, bank_s2b =
-            algo.ConsensusSequences(bank, 'bank')
-        for seq, block in pairs(bank_s2b) do
+        local query_s2b
+        query_cons, query_s2b =
+            algo.ConsensusSequences(query, 'query')
+        for seq, block in pairs(query_s2b) do
             seq2block[seq] = block
         end
         blockset = algo.Merge(query, bank)
