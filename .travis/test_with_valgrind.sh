d366180789d4d2f564169733de2bcfdc6193b5b4
Boris Nagaev <bnagaev@gmail.com>
2015-02-28 23:49:14 +0300

    disable valgrind checks for luajit
    If no memory error occurs for reference Lua,
    then probably the code of lua-npge is OK,
    so testing it in luajit with valgrind is not needed.
    
    NB. Making luajit and valgrind compatible requires
    the following configuration steps for luajit
    (not tested with lua-npge):
    
     * -DLUAJIT_USE_VALGRIND when compiling luajit
     * --suppressions=lj.supp when running valgrind
      (lj.supp can be found in luajit source tree).

--- .travis/test_with_valgrind.sh
+++ .travis/test_with_valgrind.sh
@@ -1,4 +1,6 @@
-luarocks make --local CFLAGS="-O0 -g -fPIC"
-make exitless-busted
-valgrind --error-exitcode=1 --leak-check=full \
-    lua exitless-busted
+if [ "$LUA" != "luajit" ]; then
+    luarocks make --local CFLAGS="-O0 -g -fPIC"
+    make exitless-busted
+    valgrind --error-exitcode=1 --leak-check=full \
+        lua exitless-busted
+fi
