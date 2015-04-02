7fe592694c54794caf5a25f08d78634cf5dc9c4a
Boris Nagaev <bnagaev@gmail.com>
2015-04-02 14:12:25 +0300

    valgrind: suppress leak in nsswitch

--- .travis/test_with_valgrind.sh
+++ .travis/test_with_valgrind.sh
@@ -2,5 +2,7 @@ if [ "$LUA" != "luajit" ]; then
     luarocks make --local CFLAGS="-O0 -g -fPIC"
     make exitless-busted
     valgrind --error-exitcode=1 --leak-check=full \
+        --gen-suppressions=all \
+        --suppressions=.travis/nsswitch_c_678.supp \
         lua exitless-busted
 fi
