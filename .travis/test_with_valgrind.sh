7d338e45040d8dd1c1e009605bb40ee708eb681a
Boris Nagaev <bnagaev@gmail.com>
2015-04-04 15:25:58 +0300

    travis: test with valgrind in .travis.yml

--- .travis/test_with_valgrind.sh
+++
@@ -1,8 +0,0 @@
-if [ "$LUA" != "luajit" ]; then
-    luarocks make --local CFLAGS="-O0 -g -fPIC"
-    make exitless-busted
-    valgrind --error-exitcode=1 --leak-check=full \
-        --gen-suppressions=all \
-        --suppressions=.travis/nsswitch_c_678.supp \
-        lua exitless-busted
-fi
