da073f0db1492eecb136b0174559ee24cdc97ec1
Boris Nagaev <bnagaev@gmail.com>
2015-04-12 01:53:07 +0300

    rename to_atgcn_and_gap

--- spec/alignment/to_atgcn_and_gap_spec.lua
+++ spec/alignment/to_atgcn_and_gap_spec.lua
@@ -2,9 +2,9 @@
 -- Copyright (C) 2014-2015 Boris Nagaev
 -- See the LICENSE file for terms of use.
 
-describe("npge.alignment.to_atgcn_and_gap", function()
+describe("npge.alignment.toAtgcnAndGap", function()
     it("cleans all except ATGCN and gaps", function()
-        local f = require 'npge.alignment.to_atgcn_and_gap'
+        local f = require 'npge.alignment.toAtgcnAndGap'
         assert.are.equal(f("a T g"), "ATG")
         assert.are.equal(f("a T-g"), "AT-G")
         assert.are.equal(f("a T--\ng"), "AT--G")
--- src/npge/alignment/init.lua
+++ src/npge/alignment/init.lua
@@ -2,13 +2,11 @@
 -- Copyright (C) 2014-2015 Boris Nagaev
 -- See the LICENSE file for terms of use.
 
-local to_atgcn2 = require 'npge.alignment.to_atgcn_and_gap'
-
 return {
     left = require 'npge.alignment.left',
     anchor = require 'npge.alignment.anchor',
     to_atgcn = require 'npge.alignment.to_atgcn',
-    to_atgcn_and_gap = to_atgcn2,
+    toAtgcnAndGap = require 'npge.alignment.toAtgcnAndGap',
     unwind_row = require 'npge.alignment.unwind_row',
     complement = require 'npge.alignment.complement',
     complementRows = require 'npge.alignment.complementRows',
