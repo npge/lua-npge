3e040dd9f7597e4f05fab620e0345b63d0060ea7
Boris Nagaev <bnagaev@gmail.com>
2015-04-12 14:08:54 +0300

    rename fragment_gen

--- spec/model/Fragment_spec.lua
+++ spec/model/Fragment_spec.lua
@@ -21,7 +21,7 @@ describe("npge.model.Fragment", function()
     end)
 
     local Fragment = require 'npge.model.Fragment'
-    local fragment_gen = function(seq, start, stop, ori)
+    local function fragmentGen(seq, start, stop, ori)
         return function()
             return Fragment(seq, start, stop, ori)
         end
@@ -30,19 +30,19 @@ describe("npge.model.Fragment", function()
     it("throws on bad fragment", function()
         -- linear
         local s = model.Sequence("genome&chromosome&l", "ATGC")
-        assert.has.errors(fragment_gen(nil, 1, 2, 1))
-        assert.has.errors(fragment_gen(s, 100, 110, 1))
-        assert.has.errors(fragment_gen(s, 1, 2, 10))
-        assert.has.errors(fragment_gen(s, 0, 4, 1))
-        assert.has.errors(fragment_gen(s, 2, 1, 1))
-        assert.has.errors(fragment_gen(s, 1, 2, -1))
+        assert.has.errors(fragmentGen(nil, 1, 2, 1))
+        assert.has.errors(fragmentGen(s, 100, 110, 1))
+        assert.has.errors(fragmentGen(s, 1, 2, 10))
+        assert.has.errors(fragmentGen(s, 0, 4, 1))
+        assert.has.errors(fragmentGen(s, 2, 1, 1))
+        assert.has.errors(fragmentGen(s, 1, 2, -1))
     end)
 
     it("no throw on parted fragments on circular", function()
         -- circular
         local s = model.Sequence("genome&chromosome&c", "ATGC")
-        assert.has_no.errors(fragment_gen(s, 2, 1, 1))
-        assert.has_no.errors(fragment_gen(s, 1, 2, -1))
+        assert.has_no.errors(fragmentGen(s, 2, 1, 1))
+        assert.has_no.errors(fragmentGen(s, 1, 2, -1))
     end)
 
     it("is equal #Fragment_eq", function()
