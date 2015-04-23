6602efe62d9389d5378a541c14f12dcb2ff69e9c
Boris Nagaev <bnagaev@gmail.com>
2015-04-23 17:31:48 +0300

    BlastHits: replace "subject" with "bank"
    These words are used as synonyms here.

--- src/npge/algo/BlastHits.lua
+++ src/npge/algo/BlastHits.lua
@@ -12,65 +12,62 @@ end
 
 local function readBlast(file, query, bank, same)
     local new_blocks = {}
-    local query_name, subject_name
-    local query_row, subject_row
-    local query_start, subject_start
-    local query_stop, subject_stop
+    local query_name, bank_name
+    local query_row, bank_row
+    local query_start, bank_start
+    local query_stop, bank_stop
     local function goodHit()
-        return query_name and subject_name
-            and query_row and subject_row
+        return query_name and bank_name
+            and query_row and bank_row
     end
-    local function goodFragments(query_f, subject_f)
+    local function goodFragments(query_f, bank_f)
         return true
     end
     if same then
         local goodHit0 = goodHit
         goodHit = function()
-            return goodHit0() and query_name <= subject_name
+            return goodHit0() and query_name <= bank_name
         end
-        goodFragments = function(query_f, subject_f)
-            return query_f < subject_f
+        goodFragments = function(query_f, bank_f)
+            return query_f < bank_f
         end
     end
     local function tryAdd()
         if goodHit() then
             assert(query_row)
-            assert(subject_row)
+            assert(bank_row)
             assert(query_start)
             assert(query_stop)
-            assert(subject_start)
-            assert(subject_stop)
+            assert(bank_start)
+            assert(bank_stop)
             local Fragment = require 'npge.model.Fragment'
-            local query_seq =
-                query:sequenceByName(query_name)
+            local query_seq = query:sequenceByName(query_name)
             assert(query_seq)
             local query_ori = ori(query_start, query_stop)
             local query_f = Fragment(query_seq,
                 query_start - 1, query_stop - 1, query_ori)
-            local subject_seq =
-                bank:sequenceByName(subject_name)
-            assert(subject_seq)
-            local subject_ori = ori(subject_start, subject_stop)
-            local subject_f = Fragment(subject_seq,
-                subject_start - 1, subject_stop - 1,
-                subject_ori)
-            if goodFragments(query_f, subject_f) then
+            local bank_seq = bank:sequenceByName(bank_name)
+            assert(bank_seq)
+            local bank_ori = ori(bank_start, bank_stop)
+            local bank_f = Fragment(bank_seq,
+                bank_start - 1, bank_stop - 1, bank_ori)
+            if goodFragments(query_f, bank_f) then
                 local Block = require 'npge.model.Block'
                 local query_row1 = table.concat(query_row)
-                local subject_row1 = table.concat(subject_row)
+                local bank_row1 = table.concat(bank_row)
                 local block = Block({
                     {query_f, query_row1},
-                    {subject_f, subject_row1},
+                    {bank_f, bank_row1},
                 })
                 table.insert(new_blocks, block)
             end
         end
         query_row = nil
-        subject_row = nil
+        bank_row = nil
         query_start = nil
         query_stop = nil
-        subject_start = nil
-        subject_stop = nil
+        bank_start = nil
+        bank_stop = nil
     end
     local startsWith = require 'npge.util.startsWith'
     local split = require 'npge.util.split'
@@ -88,13 +85,13 @@ local function readBlast(file, query, bank, same)
         elseif line:sub(1, 1) == '>' then
             -- Example: > consensus000567
             tryAdd()
-            subject_name = trim(line:sub(2))
-            subject_name = split(subject_name)[1]
+            bank_name = trim(line:sub(2))
+            bank_name = split(bank_name)[1]
         elseif startsWith(line, ' Score =') then
             -- Example:  Score = 82.4 bits (90),  ...
             tryAdd()
             query_row = {}
-            subject_row = {}
+            bank_row = {}
         elseif goodHit() then
             local function parseAlignment(line)
                 local parts = split(line)
@@ -122,12 +119,12 @@ local function readBlast(file, query, bank, same)
                 -- Example: Sbjct  1  GCGCG  5
                 local start, row, stop = parseAlignment(line)
                 if start and stop then
-                    if not subject_start then
-                        subject_start = assert(tonumber(start))
+                    if not bank_start then
+                        bank_start = assert(tonumber(start))
                     end
-                    subject_stop = assert(tonumber(stop))
+                    bank_stop = assert(tonumber(stop))
                 end
-                table.insert(subject_row, row)
+                table.insert(bank_row, row)
             end
         end
     end
@@ -149,7 +146,7 @@ return function(query, bank, options)
     -- possible options:
     -- - bank_fname - pre-built bank
     -- - subset - if truthy, then query is interpreted as
-    --   a subset of subject. All hits where query > subject
+    --   a subset of bank. All hits where query > bank
     --   are discarded (optimisation). They are compared
     --   as instances of Fragment.
     local Blast = require 'npge.algo.Blast'
