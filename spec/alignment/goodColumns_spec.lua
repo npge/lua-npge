d9a924984462accae1a78f6aa698f6c4af7ed763
Boris Nagaev <bnagaev@gmail.com>
2015-04-26 11:02:54 +0300

    more tests for goodColumns
    cover https://coveralls.io/builds/2423671/source?filename=src%2Fnpge%2Fcpp%2Flua_npge.cpp#L1194

--- spec/alignment/goodColumns_spec.lua
+++ spec/alignment/goodColumns_spec.lua
@@ -16,4 +16,53 @@ describe("npge.cpp.func.goodColumns", function()
             "ACTG-G",
         }), {true, false, true, false, false, true})
     end)
+
+    it("returns empty table if input is empty", function()
+        local goodColumns = require 'npge.cpp'.func.goodColumns
+        assert.same(goodColumns({}), {})
+        assert.same(goodColumns({""}), {})
+        assert.same(goodColumns({"", ""}), {})
+    end)
+
+    it("can return statuses for slice", function()
+        local goodColumns = require 'npge.cpp'.func.goodColumns
+        assert.same(goodColumns({
+            "AAT-AG",
+            "ACTGTG",
+            "ACTG-G",
+        }, 1, 3), {false, true, false})
+        assert.same(goodColumns({
+            "AAT-AG",
+            "ACTGTG",
+            "ACTG-G",
+        }, 1), {false, true, false, false, true})
+    end)
+
+    it("throws for invalid input", function()
+        local goodColumns = require 'npge.cpp'.func.goodColumns
+        assert.has_error(function()
+            goodColumns({
+                "AAT-AG",
+                "ACTGTGA",
+            })
+        end)
+        assert.has_error(function()
+            goodColumns({
+                "AAT-AG",
+                "",
+            })
+        end)
+        assert.has_error(function()
+            goodColumns({
+                "AAT-AG",
+                "ACTG-G",
+            }, -1, 2)
+        end)
+        assert.has_error(function()
+            goodColumns({
+                "AAT-AG",
+                "ACTG-G",
+            }, 0, 6)
+        end)
+    end)
 end)
