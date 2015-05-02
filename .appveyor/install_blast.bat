10605f2085c3e40d21a2b3716c9ad0390706aac0
Boris Nagaev <bnagaev@gmail.com>
2015-05-02 04:16:39 +0300

    appveyor: fix downloading zip file with curl
    Maybe the file is opened in text mode when output is redirected
    wuth ">".

--- .appveyor/install_blast.bat
+++ .appveyor/install_blast.bat
@@ -7,7 +7,7 @@ cd %APPVEYOR_BUILD_FOLDER%
 set ZIP_URL=https://github.com/npge/npge/releases/download/0.3.0/npge_0.3.0_win32.zip
 
 if not exist c:\blast\blastn.exe (
-    curl --silent --fail --max-time 120 --connect-timeout 30 %ZIP_URL% > npge_0.3.0_win32.zip
+    curl --silent --fail --max-time 120 --connect-timeout 30 --output npge_0.3.0_win32.zip %ZIP_URL%
     7z x npge_0.3.0_win32.zip
     copy npge-0.3.0\blastn.exe c:\blast
     copy npge-0.3.0\makeblastdb.exe c:\blast
