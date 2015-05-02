2f5584806c63f1076b1ef8a4b17188b1f770ec31
Boris Nagaev <bnagaev@gmail.com>
2015-05-02 15:39:05 +0300

    appveyor: create directory c:\blast

--- .appveyor/install_blast.bat
+++ .appveyor/install_blast.bat
@@ -9,6 +9,7 @@ set ZIP_URL=https://github.com/npge/npge/releases/download/0.3.0/npge_0.3.0_win3
 if not exist c:\blast\blastn.exe (
     curl --silent --fail --max-time 120 --connect-timeout 30 -L --output npge_0.3.0_win32.zip %ZIP_URL%
     7z x npge_0.3.0_win32.zip
+    mkdir c:\blast 2>NUL
     copy npge-0.3.0\blastn.exe c:\blast
     copy npge-0.3.0\makeblastdb.exe c:\blast
     copy npge-0.3.0\vcomp100.dll c:\blast
