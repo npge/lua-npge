7eb01747e446fd2b173d8d88d6838a149cdab23d
Boris Nagaev <bnagaev@gmail.com>
2015-05-08 16:18:27 +0300

    add module io/init.lua (forgotten)

---
+++ src/npge/io/init.lua
@@ -0,0 +1,20 @@
+-- lua-npge, Nucleotide PanGenome explorer (Lua module)
+-- Copyright (C) 2014-2015 Boris Nagaev
+-- See the LICENSE file for terms of use.
+
+local members = {
+    'ReadSequencesFromFasta',
+    'WriteSequencesToFasta',
+    'ReadFromBs',
+    'LoadFromLua',
+    'BlockSetToLua',
+    'ShortForm',
+}
+
+local npge_io = {}
+
+for _, member in ipairs(members) do
+    npge_io[member] = require('npge.io.' .. member)
+end
+
+return npge_io
