d24dcc0b95a7ad84c5f2629698e7b0360aa81088
Boris Nagaev <bnagaev@gmail.com>
2015-05-19 00:17:04 +0300

    Extend_spec: extends by MIN_LENGTH by default
    Cover https://coveralls.io/builds/2587993/source?filename=.%2Fsrc%2Fnpge%2Falgo%2FExtend.lua#L7

--- spec/algo/Extend_spec.lua
+++ spec/algo/Extend_spec.lua
@@ -15,4 +15,17 @@ describe("npge.algo.Extend", function()
             model.Fragment(s, 0, 2, 1),
         })}))
     end)
+
+    it("extends MIN_LENGTH positions by default", function()
+        local model = require 'npge.model'
+        local s = model.Sequence("seq", ("A"):rep(10000))
+        local f = model.Fragment(s, 5000, 5000, 1) -- middle
+        local bs = model.BlockSet({s}, {model.Block({f})})
+        local Extend = require 'npge.algo.Extend'
+        local bs2 = Extend(bs)
+        local e = require 'npge.config'.general.MIN_LENGTH
+        assert.equal(bs2, model.BlockSet({s}, {model.Block({
+            model.Fragment(s, 5000 - e, 5000 + e, 1),
+        })}))
+    end)
 end)
