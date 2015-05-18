e2de76237c566284fb9fea77207ce7c72e275837
Boris Nagaev <bnagaev@gmail.com>
2015-05-18 13:42:53 +0300

    algo.Extend gets length as single number argument
    Do not use length-returning function here.

--- spec/algo/Extend_spec.lua
+++ spec/algo/Extend_spec.lua
@@ -10,26 +10,7 @@ describe("npge.algo.Extend", function()
         local f = model.Fragment(s, 1, 1, 1)
         local bs = model.BlockSet({s}, {model.Block({f})})
         local Extend = require 'npge.algo.Extend'
-        local bs2 = Extend(bs, function(block)
-            assert(block:type() == 'Block')
-            return 1, 2
-        end)
-        assert.equal(bs2, model.BlockSet({s}, {model.Block({
-            model.Fragment(s, 0, 3, 1),
-        })}))
-    end)
-
-    it("single length = left = right",
-    function()
-        local model = require 'npge.model'
-        local s = model.Sequence("seq", "ATGC")
-        local f = model.Fragment(s, 1, 1, 1)
-        local bs = model.BlockSet({s}, {model.Block({f})})
-        local Extend = require 'npge.algo.Extend'
-        local bs2 = Extend(bs, function(block)
-            assert(block:type() == 'Block')
-            return 1
-        end)
+        local bs2 = Extend(bs, 1)
         assert.equal(bs2, model.BlockSet({s}, {model.Block({
             model.Fragment(s, 0, 2, 1),
         })}))
--- src/npge/algo/Extend.lua
+++ src/npge/algo/Extend.lua
@@ -2,14 +2,16 @@
 -- Copyright (C) 2014-2015 Boris Nagaev
 -- See the LICENSE file for terms of use.
 
-return function(blockset, lengthsGetter)
-    local extend = require 'npge.block.extend'
-    local BlockSet = require 'npge.model.BlockSet'
-    local new_blocks = {}
-    for block in blockset:iterBlocks() do
-        local left_length, right_length = lengthsGetter(block)
-        block = extend(block, left_length, right_length)
-        table.insert(new_blocks, block)
+return function(blockset, max_length)
+    if not max_length then
+        max_length = require 'npge.config'.general.MIN_LENGTH
     end
+    local new_blocks = {}
+    local extend = require 'npge.block.extend'
+    for block in blockset:iterBlocks() do
+        local b = assert(extend(block, max_length))
+        table.insert(new_blocks, b)
+    end
+    local BlockSet = require 'npge.model.BlockSet'
     return BlockSet(blockset:sequences(), new_blocks)
 end
