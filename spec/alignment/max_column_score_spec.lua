334b6ec56732a9d812af3fa5d52d7d85c074219c
Boris Nagaev <bnagaev@gmail.com>
2015-10-21 11:37:43 +0300

    typo in test name

--- spec/alignment/max_column_score_spec.lua
+++ spec/alignment/max_column_score_spec.lua
@@ -3,7 +3,7 @@
 -- See the LICENSE file for terms of use.
 
 describe("npge.cpp.alignment.MAX_COLUMN_SCORE", function()
-    it("is rqual to 100", function()
+    it("is equal to 100", function()
         local cpp = require 'npge.cpp'
         assert.equal(cpp.alignment.MAX_COLUMN_SCORE, 100)
     end)
