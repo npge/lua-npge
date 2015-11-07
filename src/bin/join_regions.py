c02259e01420707caeb31e285bf2e9f2589b20e7
Boris Nagaev <bnagaev@gmail.com>
2015-11-07 15:19:18 +0300

    join_regions.py: apply again with frame=min_length

--- src/bin/join_regions.py
+++ src/bin/join_regions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import copy
+import os
+import os.path
 import sys
 
 # TODO
@@ -14,9 +16,14 @@ def read_regions(input_file):
         fields = line.split()
         if len(fields) >= 2 and fields[0] in '+-':
             region = {
-                "length": int(fields[1]),
                 "type": fields[0],
+                "orig_type": fields[0],
+                "length": int(fields[1]),
             }
+            if region["type"] == "+":
+                region["good_length"] = region["length"]
+            else:
+                region["good_length"] = 0
             regions.append(region)
     return regions
 
@@ -30,7 +37,7 @@ def join_regions(regions, min_identity, min_length, frame):
         for region in regions_between:
             if region["type"] == '-' and region["length"] >= frame:
                 return False
-        good_between = [region["length"] for region in regions_between if region["type"] == '+']
+        good_between = [region["good_length"] for region in regions_between if region["type"] == '+']
         length_between = [region["length"] for region in regions_between]
         good_all = sum(good_between) + 2 * frame
         length_all = sum(length_between) + 2 * frame
@@ -39,7 +46,6 @@ def join_regions(regions, min_identity, min_length, frame):
         regions_between = [regions[i] for i in range(prev_good + 1, next_good)]
         change_to_good = is_good_group(regions_between)
         for region in regions_between:
-            region["orig_type"] = region["type"]
             if change_to_good:
                 region["type"] = '+'
             else:
@@ -52,52 +58,55 @@ def fold(regions):
     for region in regions:
         if prev and region["type"] == prev["type"]:
             prev["length"] += region["length"]
+            prev["good_length"] += region["good_length"]
         else:
             prev = copy.copy(region)
-            prev["good_length"] = 0
+            # orig_type = orig_type of first block
             joined_regions.append(prev)
-        orig_type = region["type"]
-        if "orig_type" in region:
-            orig_type = region["orig_type"]
-        if orig_type == '+':
-            prev["good_length"] += region["length"]
     return joined_regions
 
-def print_joined(joined_regions, regions, g):
-    assert len(joined_regions) == len(regions)
-    for joined_region, region in zip(joined_regions, regions):
-        assert joined_region["length"] == region["length"]
-        g.write("%s\t%d\t%s\n" % (region["type"], region["length"], joined_region["type"]))
-
-def print_regions(regions, g):
+def print_regions(regions, g, print_orig_type):
     for region in regions:
-        if "good_length" in region:
-            mut = region["length"] - region["good_length"]
-            g.write("%s\t%d\t%d\n" % (region["type"], region["length"], mut))
+        mut = region["length"] - region["good_length"]
+        if print_orig_type:
+            g.write("%s\t%d\t%s\t%d\n" % (region["type"], region["length"], region["orig_type"], mut))
         else:
-            g.write("%s\t%d\n" % (region["type"], region["length"]))
-
-_dir = "step1/"
+            g.write("%s\t%d\t%d\n" % (region["type"], region["length"], mut))
 
 if __name__ == '__main__':
     min_length = int(sys.argv[1])
     min_identity = float(sys.argv[2])
     # -1 because mutation is not counted
     frame = max(3, int(round(1.0 / (1.0 - min_identity) - 1)))
-    full = int(sys.argv[3])
+    step = int(sys.argv[3])
     infile = sys.argv[4]
     f = open(infile)
     for x in f:
         if len(x.strip()) == 0:
             continue
         h = open(x.strip())
-        regions = read_regions(h)
-        joined_regions = join_regions(regions, min_identity, min_length, frame)
-        g = open(_dir + x.strip(),"w")
-        if full:
-            print_joined(joined_regions, regions, g)
-        else:
-            joined_regions = fold(joined_regions)
-            print_regions(joined_regions, g)
+        regions0 = read_regions(h)
+        regions1 = join_regions(regions0, min_identity, min_length, frame)
+        dir = "step%d" % step
+        try:
+            os.mkdir(dir)
+        except:
+            pass
+        fname = os.path.join(dir, x.strip())
+        g = open(fname, 'w')
+        if step == 1:
+            print_regions(regions1, g, print_orig_type=True)
+        elif step == 2:
+            regions2 = fold(regions1)
+            print_regions(regions2, g, print_orig_type=False)
+        elif step == 3:
+            regions2 = fold(regions1)
+            regions3 = join_regions(regions2, min_identity, min_length, frame=min_length)
+            print_regions(regions3, g, print_orig_type=True)
+        elif step == 4:
+            regions2 = fold(regions1)
+            regions3 = join_regions(regions2, min_identity, min_length, frame=min_length)
+            regions4 = fold(regions3)
+            print_regions(regions4, g, print_orig_type=False)
         g.close()
         h.close()
