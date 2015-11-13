32cae86febb910b1d82096b118c7e6b15724dc4d
Boris Nagaev <bnagaev@gmail.com>
2015-11-13 11:21:28 +0300

    ReadFromBs: refactoring

--- src/npge/io/ReadFromBs.lua
+++ src/npge/io/ReadFromBs.lua
@@ -2,21 +2,24 @@
 -- Copyright (C) 2014-2015 Boris Nagaev
 -- See the LICENSE file for terms of use.
 
-local function readWithReference(generator, bs1)
-    local blockname2fragments = {}
-    local ev = require 'npge.util.extractValue'
-    local parseId = require 'npge.fragment.parseId'
-    for name, description, text in generator do
-        local blockname = assert(ev(description, "block"),
-            ("No block name found in %q"):format(description))
-        local seqname, start, stop, ori = parseId(name)
-        if not blockname2fragments[blockname] then
-            blockname2fragments[blockname] = {}
+local function readWithReference(bs1)
+    return function(generator)
+        local blockname2fragments = {}
+        local ev = require 'npge.util.extractValue'
+        local parseId = require 'npge.fragment.parseId'
+        for name, description, text in generator do
+            local blockname = ev(description, "block")
+            if blockname then
+                local seqname, start, stop, ori = parseId(name)
+                if not blockname2fragments[blockname] then
+                    blockname2fragments[blockname] = {}
+                end
+                local fr_desc = {seqname, start, stop, ori, text}
+                table.insert(blockname2fragments[blockname], fr_desc)
+            end
         end
-        local fr_desc = {seqname, start, stop, ori, text}
-        table.insert(blockname2fragments[blockname], fr_desc)
+        return bs1:sequences(), blockname2fragments
     end
-    return bs1:sequences(), blockname2fragments
 end
 
 local function makeBlock(bs1, fr_descs)
@@ -39,15 +42,15 @@ return function(lines, blockset_with_sequences)
         local util = require 'npge.util'
         lines = util.textToIt(lines)
     end
-    local bs1 = blockset_with_sequences
     local fromFasta = require 'npge.util.fromFasta'
-    local f = readWithReference
-    local sequences, blockname2fragments = f(fromFasta(lines), bs1)
+    local f = readWithReference(blockset_with_sequences)
+    local sequences, blockname2fragments = f(fromFasta(lines))
+    local BlockSet = require 'npge.model.BlockSet'
+    local bs1 = BlockSet(sequences, {})
     local blocks = {}
     for blockname, fr_descs in pairs(blockname2fragments) do
         local Block = require 'npge.model.Block'
         blocks[blockname] = makeBlock(bs1, fr_descs)
     end
-    local BlockSet = require 'npge.model.BlockSet'
-    return BlockSet(bs1:sequences(), blocks)
+    return BlockSet(sequences, blocks)
 end
