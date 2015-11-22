235ead6ab33872a27695b47fdd9a51153cb8c312
Boris Nagaev <bnagaev@gmail.com>
2015-11-23 01:15:35 +0300

    reimplement isSubfragmentOf() using overlaps()

--- src/npge/fragment/isSubfragmentOf.lua
+++ src/npge/fragment/isSubfragmentOf.lua
@@ -4,34 +4,11 @@
 
 return function(self, source)
     assert(self:sequence() == source:sequence())
-    local hasPos = require 'npge.fragment.hasPos'
-    if not hasPos(source, self:start())
-            or not hasPos(source, self:stop()) then
-        return false
+    local overlaps = require 'npge.fragment.overlaps'
+    local o = overlaps(self, source)
+    local length = 0
+    for _, f in ipairs(o) do
+        length = length + f:length()
     end
-    if not source:parted() and not self:parted() then
-        return true
-    elseif source:length() == source:sequence():length() then
-        -- source covers whole sequence
-        return true
-    else
-        -- check all boundaries of source
-        local points = {source:start(), source:stop()}
-        local points1 = {}
-        for _, point in ipairs(points) do
-            table.insert(points1, point - 1)
-            table.insert(points1, point)
-            table.insert(points1, point + 1)
-        end
-        for _, point in ipairs(points1) do
-            local fixPosition =
-                require 'npge.sequence.fixPosition'
-            point = fixPosition(self:sequence(), point)
-            if hasPos(self, point) and
-                    not hasPos(source, point) then
-                return false
-            end
-        end
-        return true
-    end
+    return length == self:length()
 end
