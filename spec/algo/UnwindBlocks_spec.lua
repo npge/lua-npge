baa8c77177088ac3720c7654fecf52208ad0babd
Boris Nagaev <bnagaev@gmail.com>
2016-02-10 09:14:34 +0300

    update Copyright year to 2014-2016
    For refineAlignment.cpp: 2012-2016

--- spec/algo/UnwindBlocks_spec.lua
+++ spec/algo/UnwindBlocks_spec.lua
@@ -1,5 +1,5 @@
 -- lua-npge, Nucleotide PanGenome explorer (Lua module)
--- Copyright (C) 2014-2015 Boris Nagaev
+-- Copyright (C) 2014-2016 Boris Nagaev
 -- See the LICENSE file for terms of use.
 
 describe("npge.algo.UnwindBlocks", function()
