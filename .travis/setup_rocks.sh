698ef33a300a891526035d469d7206241343804f
Boris Nagaev <bnagaev@gmail.com>
2016-02-11 23:02:40 +0300

    Travis: use luacov-coveralls 0.1.1
    New luacov-coveralls (0.2.0) produces empty coveralls reports.
    
    See https://coveralls.io/builds/5045163
    See https://github.com/moteus/luacov-coveralls/issues/1

--- .travis/setup_rocks.sh
+++ .travis/setup_rocks.sh
@@ -1,6 +1,6 @@
 luarocks install busted --local
 luarocks install luacov --local
-luarocks install luacov-coveralls --local
+luarocks install luacov-coveralls 0.1.1 --local
 luarocks install lua-llthreads2 --local
 luarocks install luaposix --local
 luarocks install lua-rote --local
