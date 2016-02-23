036c49a3624d639d61ad4bcf6c30a951f5898bf1
Boris Nagaev <bnagaev@gmail.com>
2016-02-23 03:40:21 +0300

    check sources with luacheck in Travis
    Remove lua-globals.sh. It does only a check against globals.

--- .travis/lua-globals.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-(for f in $@; do luac -l -p $f; done) | egrep ETGLOBAL | \
-    awk '{print $7}' | \
-    ( ! egrep -wv \
-    "string|xpcall|package|tostring|print|os|unpack|require|getfenv|setmetatable|next|assert|tonumber|io|rawequal|collectgarbage|getmetatable|module|rawset|math|debug|pcall|table|newproxy|type|coroutine|_G|select|gcinfo|pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|load|error|loadfile" )
